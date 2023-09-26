252d8bb51c030635e02d63e36b5f645bf70a006a
tainguyen58ntt <tainguyen58ntt@gmail.com>
2023-09-26 11:19:50 +0700

    Test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scholarit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public string Test()
+        {
+            return "sdfdsf";
+        }
+    }
+}
